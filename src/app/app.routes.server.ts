8a51415e65f947f69647713f99312c4e4937d615
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2025-02-13 16:42:44 +0200

    .

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,11 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+// import { RenderMode, ServerRoute } from '@angular/ssr';
+
+// export const serverRoutes: ServerRoute[] = [
+//   {
+
+//   }
+
 export const serverRoutes: ServerRoute[] = [
   {
-    // path: '**',
-    // renderMode: RenderMode.Prerender,
-
-    path: '',
-    renderMode: RenderMode.Client,
+    path: '**',
+    renderMode: RenderMode.Prerender,
   },
 ];
